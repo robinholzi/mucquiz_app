32f108bc72629a2d184215eb236032fedd39d8ca
Robin Holzinger <rnholzinger01@gmail.com>
2021-11-20 13:45:37 +0100

    fixed compilation error

--- lib/root.dart
+++ lib/root.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:munich_data_quiz/constants/text.dart';
+import 'package:munich_data_quiz/pages/topic_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
