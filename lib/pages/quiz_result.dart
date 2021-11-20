a065ae8c527be048ba167080a017fa773ac45eb2
xarantolus <xarantolus@protonmail.com>
2021-11-20 16:23:44 +0100

    Basic result page

--- lib/pages/quiz_result.dart
+++ lib/pages/quiz_result.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:munich_data_quiz/api/models.dart';
 import 'package:munich_data_quiz/api/quiz_api.dart';
+import 'package:munich_data_quiz/widgets/image_widget.dart';
 
 class QuizResultPage extends StatefulWidget {
   const QuizResultPage(this.quiz, this.submission, {Key? key})
@@ -15,13 +16,81 @@ class QuizResultPage extends StatefulWidget {
 }
 
 class _QuizResultPageState extends State<QuizResultPage> {
+  static const titleStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  Widget _answer(
+      BuildContext context, QuizAnswer answer, EvaluatedQuestion result) {
+    var isCorrect = false;
+    if (result.incorrectAnswers?.isNotEmpty ?? false) {
+      var answers =
+          result.incorrectAnswers!.where((element) => element.id == answer.id);
+
+      isCorrect = answers.isNotEmpty ? answers.first.correct ?? false : false;
+    }
+
+    return CheckboxListTile(
+      title: Text(
+        answer.text ?? "",
+        style: TextStyle(
+          decoration: TextDecoration.lineThrough,
+        ),
+      ),
+      value: isCorrect,
+      onChanged: null,
+    );
+  }
+
+  Widget _singleQuestionResult(
+      QuizQuestion question, EvaluatedQuestion result) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          question.title,
+          style: titleStyle,
+          textAlign: TextAlign.center,
+        ),
+        if ((question.imgUrl ?? "").isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ImageWidget(question.imgUrl!),
+          ),
+        if ((question.description ?? "").isNotEmpty)
+          Text(
+            question.description!,
+            textAlign: TextAlign.center,
+          ),
+        ...question.answers.map((a) => _answer(context, a, result))
+      ],
+    );
+  }
+
+  Widget _quizResultList(QuizSubmissionResponse response) {
+    return ListView.builder(
+      itemCount: widget.quiz.questions.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _singleQuestionResult(
+          widget.quiz.questions[index],
+          response.data!.firstWhere(
+            (element) => element.questionId == widget.quiz.questions[index].id,
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.resultPageTitle),
       ),
-      body: Center(
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(16),
         child: FutureBuilder<QuizSubmissionResponse>(
           future: QuizAPI().evaluateQuizTotal(widget.submission),
           builder: (context, snapshot) {
@@ -29,7 +98,7 @@ class _QuizResultPageState extends State<QuizResultPage> {
               return ErrorWidget(snapshot.error!);
             }
             if (snapshot.hasData) {
-              return Text("${snapshot.data!.data!.length}");
+              return _quizResultList(snapshot.data!);
             }
 
             return const CircularProgressIndicator();
